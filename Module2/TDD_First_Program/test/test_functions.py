30f1af207e2dd36d837a0186c94db2cb69b80942
Levi Shepherd <levi.d.shepherd@outlook.com>
2020-09-07 21:42:41 -0500

    added TDD_First_Program to Python repo

---
+++ Module2/TDD_First_Program/test/test_functions.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def convert_to_months(self):
+        self.assertEqual(False, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
