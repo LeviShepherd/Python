221090954466d6e45841e8432c48b4da3daf8dc4
Levi Shepherd <levi.d.shepherd@outlook.com>
2020-09-07 22:17:49 -0500

    added finished product
    finished out convert_to_months and expanded the testing of the function and added print statement

--- TDD_First_program/main/camper_age_input.py
+++ TDD_First_program/main/camper_age_input.py
@@ -1,6 +1,28 @@
-def convert_to_months():
-    pass
+"""
+Program: camper_age_input.py
+Author: Levi Shepherd
+Last date modified: 09/07/2020
+
+
+
+The purpose of this program is to accept input of
+a child's age in years and then output the age of the
+child in months.
+"""
+
+
+def convert_to_months(years):
+    MONTHS_IN_YEAR = 12
+    months = years * MONTHS_IN_YEAR
+    return months
 
 
 if __name__ == '__main__':
-    print()
+    # Ask user for input of child's age and cast the input to an integer
+    age_in_years = int(input("Enter the child's age in years: "))
+
+    # Pass the input value to the function to calculate age to months
+    age_in_months = convert_to_months(age_in_years)
+
+    # Display the input of years and output of months to user
+    print(str(age_in_years) + " years is " + str(age_in_months) + " months.")
--- TDD_First_program/test/test_functions.py
+++ TDD_First_program/test/test_functions.py
@@ -3,8 +3,10 @@ from TDD_First_program.main import camper_age_input
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEquals(False, True)
+    def test_convert_to_months(self):
+        expected = 60
+        actual = camper_age_input.convert_to_months(5)
+        self.assertEquals(expected, actual)
 
 
 if __name__ == '__main__':
