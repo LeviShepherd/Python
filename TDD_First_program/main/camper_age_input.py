21aa8e95043bdd77c4f8e675f988761fda15d69c
Levi Shepherd <levi.d.shepherd@outlook.com>
2020-09-07 22:13:15 -0500

    added tdd project

---
+++ TDD_First_program/main/camper_age_input.py
@@ -0,0 +1,6 @@
+def convert_to_months():
+    pass
+
+
+if __name__ == '__main__':
+    print()
---
+++ TDD_First_program/test/test_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from TDD_First_program.main import camper_age_input
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEquals(False, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
